408aa51f63599548b2a1e274484031dde5608df0
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2024-01-01 09:35:16 +0600

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
+import { Poppins } from 'next/font/google';
+
+import { LoginButton } from '@/components/auth/login-button';
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600'],
+});
 
 export default function Home() {
   return (
-    <section>
-      <h1 className='font-montserrat text-purple-500'>RS Shonjoy</h1>
-      <Button variant='ghost'>Redolence</Button>
-    </section>
+    <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+      <div className='space-y-6 text-center'>
+        <h1 className={cn('text-6xl font-semibold text-white drop-shadow-md', font.className)}>
+          🔐 Auth
+        </h1>
+        <p className='text-lg text-white'>A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant='secondary' size='lg'>
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
