44714fc15bdd9d0e3bdd5d1894e56fe26a98d88a
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2024-01-01 19:20:48 +0600

    feat: create user

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,10 @@
 'use server';
 
+import bcrypt from 'bcryptjs';
 import * as z from 'zod';
 
+import { getUserByEmail } from '@/data/user';
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -10,6 +13,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: 'Invalid fields!' };
   }
+
+  const { name, email, password } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
   return { success: 'Email sent!' };
 };
