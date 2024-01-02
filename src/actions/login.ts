312984c7b499662b58d7c4c829bd043846f9bd47
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2024-01-02 11:51:23 +0600

    feat: resend

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -4,6 +4,9 @@ import { AuthError } from 'next-auth';
 import * as z from 'zod';
 
 import { signIn } from '@/auth';
+import { getUserByEmail } from '@/data/user';
+import { sendVerificationEmail } from '@/lib/mail';
+import { generateVerificationToken } from '@/lib/tokens';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { LoginSchema } from '@/schemas';
 
@@ -15,6 +18,20 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   }
 
   const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: 'Invalid credentials' };
+  }
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(existingUser.email);
+
+    await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+    return { success: 'Confirmation email sent!' };
+  }
 
   try {
     await signIn('credentials', {
--- src/actions/register.ts
+++ src/actions/register.ts
@@ -5,6 +5,8 @@ import * as z from 'zod';
 
 import { getUserByEmail } from '@/data/user';
 import { db } from '@/lib/db';
+import { sendVerificationEmail } from '@/lib/mail';
+import { generateVerificationToken } from '@/lib/tokens';
 import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -32,5 +34,9 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     },
   });
 
-  return { success: 'Email sent!' };
+  const verificationToken = await generateVerificationToken(email);
+
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+  return { success: 'confirmation email sent!' };
 };
